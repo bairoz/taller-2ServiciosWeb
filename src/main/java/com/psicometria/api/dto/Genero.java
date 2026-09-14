02831f1501d7f90bd5c1ec9380c628eb8e6f6672
bairoz <byrodr1ortega@gmail.com>
2026-09-14 14:51:44 -0600

    Agregar EvaluadoDTO con validaciones y enums Genero y NivelEducativo

---
+++ src/main/java/com/psicometria/api/dto/Genero.java
@@ -0,0 +1,8 @@
+package com.psicometria.api.dto;
+
+public enum Genero {
+    MASCULINO,
+    FEMENINO,
+    NO_BINARIO,
+    PREFIERO_NO_DECIR
+}
---
+++ src/main/java/com/psicometria/api/dto/NivelEducativo.java
@@ -0,0 +1,9 @@
+package com.psicometria.api.dto;
+
+public enum NivelEducativo {
+    BASICA,
+    MEDIA,
+    TECNICO,
+    UNIVERSITARIO,
+    POSTGRADO
+}
